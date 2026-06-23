c3ea594707d6391cfd33f65547fa12e361e8666d
emelyanovas85 <98567068+emelyanovas85@users.noreply.github.com>
2026-06-23 14:23:21 +0300

    fix: update test annotations for Spring Boot 4.x package changes

--- src/test/java/ru/cbr/bugbusters/gitwebhookhandler/persistence/WebhookEventRepositoryTest.java
+++ src/test/java/ru/cbr/bugbusters/gitwebhookhandler/persistence/WebhookEventRepositoryTest.java
@@ -3,7 +3,7 @@ package ru.cbr.bugbusters.gitwebhookhandler.persistence;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import ru.cbr.bugbusters.gitwebhookhandler.persistence.entity.WebhookEventEntity;
 import ru.cbr.bugbusters.gitwebhookhandler.persistence.repository.WebhookEventRepository;
