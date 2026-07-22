a69d3190bbbcf269728758f3bd4e00c32a35af24
emelyanov_as <emelyanovas85@mail.ru>
2026-07-22 16:53:11 +0300

    изменение таймаутов

--- src/main/java/ru/cbr/bugbusters/gitwebhookhandler/common/config/ClientConfig.java
+++ src/main/java/ru/cbr/bugbusters/gitwebhookhandler/common/config/ClientConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestClient;
+import ru.cbr.bugbusters.gitwebhookhandler.review.service.LlmRateLimiter;
 
 import java.time.Duration;
 
